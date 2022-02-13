b3e8f5a0af8c714e65c1042e7f82bfcdcc92d864
matheus.ribeiro <matheus.ribeiro@oab.org.br>
2022-02-13 18:32:10 -0300

    first commit

---
+++ BackEnd/src/SistemaFinanceiro.API/Controllers/LucroController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using SistemaFinanceiro.API.Models;
+
+namespace SistemaFinanceiro.API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class LucroController : ControllerBase
+    {
+        public LucroController()
+        {
+            
+        }
+
+        [HttpGet]
+        public Lucro Get()
+        {
+            return new Lucro(){
+                Id = 1,
+                Nome = "Salário",
+                Valor = 3600
+            };
+        }
+    }
+}
---
+++ BackEnd/src/SistemaFinanceiro.API/Models/Lucro.cs
@@ -0,0 +1,10 @@
+namespace SistemaFinanceiro.API.Models
+{
+    public class Lucro
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public float Valor { get; set; }
+        
+    }
+}
